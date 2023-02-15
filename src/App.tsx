fe65dd05d9bd4af0a188291223510808eec7e86e
Julian Berner <Julian-Berner@outlook.de>
2023-02-15 23:07:40 +0100

    adding wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import QuestionnaireGraphPage from "./pages/QuestionnaireGraphPage";
+import { Route } from "wouter";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
   return (
     <div className="flex h-screen flex-col overflow-hidden">
-      <QuestionnaireGraphPage />
+      <Route path="">
+        <IndexPage />
+      </Route>
+      <Route path="/graph">
+        <QuestionnaireGraphPage />
+      </Route>
     </div>
   );
 }
