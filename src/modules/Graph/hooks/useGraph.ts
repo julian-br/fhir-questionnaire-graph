298359492fa2a2dbdbfec58cef3330fa5fab09c5
Julian Berner <102370526+julian-br@users.noreply.github.com>
2023-02-16 23:15:04 +0100

    Reactflow
    * dynamic layout kind of working
    
    * improving dynamic layout
    
    * only run layouting once
    
    * temporarly setting params of calc graph to any
    
    * using questionnaire data fpr nodes
    
    * moving graph states into custom hook
    
    * fixing typeScript errors in calcGraphLayout
    
    * cleanup
    
    * moving graph into own module
    
    * fixing typescript errors
    
    * renaming flow into graph
    
    * fixing alignment of side bar button when the sidebar is closed
    
    * calculating graph layout with elk instead of dagre
    
    * setting opacity to zero when graph is layouting
    
    * resetting viewport when layouted
    
    * restricting max zoom
    
    * fixing edge gap because invisible port

---
+++ src/modules/Graph/hooks/useGraph.ts
@@ -0,0 +1,110 @@
+import { QuestionnaireItem } from "fhir/r4";
+import { useEffect, useState } from "react";
+import { FHIRQuestionnaire } from "../../../fhir-questionnaire/FHIRQuestionnaire";
+import {
+  Edge,
+  Node,
+  useEdgesState,
+  useNodesInitialized,
+  useNodesState,
+} from "reactflow";
+import { Layout } from "../calcGraphLayout";
+import { NodeData } from "../components/CustomNode";
+
+const NODE_WIDTH = 350;
+
+export default function useGraph(
+  questionnaire: FHIRQuestionnaire,
+  activeItemId: string
+) {
+  const [nodes, setNodes] = useNodesState<NodeData>(
+    createNodes(questionnaire, activeItemId)
+  );
+  const [edges, setEdges] = useEdgesState<Edge>(
+    createEdges(questionnaire, activeItemId)
+  );
+  const [isLayouted, setIsLayouted] = useState(false);
+
+  useEffect(() => {
+    setIsLayouted(false);
+    setNodes(createNodes(questionnaire, activeItemId));
+    setEdges(createEdges(questionnaire, activeItemId));
+  }, [activeItemId, questionnaire]);
+
+  function setLayout(layout: Layout) {
+    setNodes(layout.layoutedNodes);
+    setEdges(layout.layoutedEdges);
+    setIsLayouted(true);
+  }
+
+  return {
+    nodes,
+    edges,
+    setLayout,
+    isLayouted,
+  };
+}
+
+export function createNodes(
+  questionnaire: FHIRQuestionnaire,
+  itemLinkId: string
+) {
+  const nestedItems = questionnaire.getNestedItems(itemLinkId);
+
+  if (nestedItems.length === 0) {
+    const item = questionnaire.getItemById(itemLinkId);
+    return [createNodeFromItem(item!)];
+  }
+
+  const foreignItems = questionnaire.getForeignDependendNestedItems(itemLinkId);
+  const foreignItemsNodes = foreignItems.map((foreignItem) => {
+    const foreignItemGroupId = questionnaire.getGroupIdOfItem(
+      foreignItem.linkId
+    );
+    return createNodeFromItem(foreignItem, true, foreignItemGroupId);
+  });
+
+  const nestedItemsNodes = nestedItems.map((nestedItem) =>
+    createNodeFromItem(nestedItem)
+  );
+
+  return [...nestedItemsNodes, ...foreignItemsNodes];
+}
+
+function createNodeFromItem(
+  item: QuestionnaireItem,
+  isForeign: boolean = false,
+  foreignItemGroupId?: string
+): Node<NodeData> {
+  return {
+    id: item.linkId,
+    width: NODE_WIDTH,
+    data: {
+      isForeign,
+      foreignItemGroupId,
+      itemData: item,
+    },
+    type: "custom",
+    position: {
+      x: NaN,
+      y: NaN,
+    },
+  };
+}
+
+export function createEdges(
+  questionnaire: FHIRQuestionnaire,
+  itemLinkId: string
+) {
+  const nestedItemsWithDependency =
+    questionnaire.getNestedItemsWithDependency(itemLinkId);
+
+  return nestedItemsWithDependency.map((itemWithDependency) => {
+    return {
+      id:
+        itemWithDependency.linkId + itemWithDependency.enableWhen![0].question,
+      source: itemWithDependency.enableWhen![0].question,
+      target: itemWithDependency.linkId,
+    };
+  });
+}
