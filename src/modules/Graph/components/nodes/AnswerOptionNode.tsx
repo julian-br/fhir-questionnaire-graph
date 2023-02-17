2d98e70424d943d601275062ff58c003271b4e35
Julian Berner <Julian-Berner@outlook.de>
2023-02-17 20:20:23 +0100

    adding graph controls

--- src/modules/Graph/components/nodes/AnswerOptionNode.tsx
+++ src/modules/Graph/components/nodes/AnswerOptionNode.tsx
@@ -7,7 +7,7 @@ export interface AnswerNodeData extends QuestionnaireItemAnswerOption {}
 export default function AnswerOptionNode({ data }: NodeProps<AnswerNodeData>) {
   return (
     <NodeContainer>
-      <div className="w-44   bg-secondary-light p-2 text-sm text-slate-800">
+      <div className="w-48   bg-secondary-light p-2 text-sm text-slate-800">
         <p>{data.valueString}</p>
         {data.initialSelected === true && (
           <p className="mt-1 w-fit rounded border border-green-700 bg-green-50 px-1 text-green-700">
