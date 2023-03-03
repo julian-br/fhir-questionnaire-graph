bbb610e9660bf3881cb6c9bb87f13c9ea27f085d
Julian Berner <Julian-Berner@outlook.de>
2023-03-03 18:24:39 +0100

    displaying item info in modal

--- src/components/ViewItemModal.tsx
+++ src/components/ViewItemModal.tsx
@@ -1,11 +1,38 @@
+import { QuestionnaireItem } from "fhir/r4";
 import TextArea from "./common/input/TextArea";
 import Modal from "./common/Modal";
 
 interface ViewItemModalProps {
   onClose: () => void;
+  item: QuestionnaireItem;
 }
 
-export default function ViewItemModal({ onClose }: ViewItemModalProps) {
+export default function ViewItemModal({ onClose, item }: ViewItemModalProps) {
+  return (
+    <Modal onClose={onClose}>
+      <div className="">
+        <div className="">
+          <span className="text-sm text-slate-500">{item.linkId}</span>
+          <h5 className="pb-4 text-lg">
+            {item.prefix !== undefined && (
+              <span className="pr-2 font-medium">{item.prefix}</span>
+            )}
+            {item.text}
+          </h5>
+
+          <div>
+            <h6 className="font-medium">Annotations</h6>
+            <AnnotationFeed />
+          </div>
+
+          <TextArea placeholder="Add a new annotation..." fitContent={true} />
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+function AnnotationFeed() {
   const activityItems = [
     {
       id: 1,
@@ -21,45 +48,22 @@ export default function ViewItemModal({ onClose }: ViewItemModalProps) {
     },
   ];
   return (
-    <Modal onClose={onClose}>
-      <div className="">
-        <div className="">
-          <span className="text-sm text-slate-500">Link12141.1232.base.ff</span>
-          <h5 className="pb-4 text-xl">
-            <span className="font-medium">3.1</span> Waren sie schonmal
-            schwanger
-          </h5>
-
-          <div>
-            <h6 className="font-medium">Annotations</h6>
-            <div>
-              <ul role="list" className="divide-y divide-gray-200">
-                {activityItems.map((activityItem) => (
-                  <li key={activityItem.id} className="py-4">
-                    <div className="flex space-x-3">
-                      <div className="flex-1 space-y-1">
-                        <div className="flex items-center justify-between">
-                          <h3 className="text-sm font-medium">
-                            {activityItem.name}
-                          </h3>
-                          <p className="text-sm text-gray-500">
-                            {activityItem.time}
-                          </p>
-                        </div>
-                        <p className="text-sm text-gray-500">
-                          {activityItem.text}
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+    <div>
+      <ul role="list" className="divide-y divide-gray-200">
+        {activityItems.map((activityItem) => (
+          <li key={activityItem.id} className="py-3">
+            <div className="flex space-x-3">
+              <div className="flex-1 space-y-1">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-sm font-medium">{activityItem.name}</h3>
+                  <p className="text-sm text-gray-500">{activityItem.time}</p>
+                </div>
+                <p className="text-sm text-gray-500">{activityItem.text}</p>
+              </div>
             </div>
-          </div>
-
-          <TextArea placeholder="Add a new annotation..." fitContent={true} />
-        </div>
-      </div>
-    </Modal>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/pages/GraphPage.tsx
+++ src/pages/GraphPage.tsx
@@ -73,7 +73,10 @@ export default function GraphPage({
           )}
 
           {selectedItem !== null && (
-            <ViewItemModal onClose={() => setSelectedItem(null)} />
+            <ViewItemModal
+              item={selectedItem}
+              onClose={() => setSelectedItem(null)}
+            />
           )}
         </main>
       )}
